b85b68d6b62fbe90a2335680b10598cc6f938c50
Robbie Marcelo <rbmrclo@hotmail.com>
2018-02-06 20:14:47 +0800

    Setup vars.tf

---
+++ vars.tf
@@ -0,0 +1,13 @@
+variable "region" {
+  description = "The region where to deploy this code (e.g. us-east-1)."
+  default = "us-east-1"
+}
+
+variable "access_key" {
+  default = "" # Enter your aws access key id here
+}
+
+variable "secret_key" {
+  default = "" # Enter your aws secrey key id here
+}
+
