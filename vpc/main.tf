5cc3f8a888366631346b3c242b1bd9d20c6aeef3
Robbie Marcelo <rbmrclo@hotmail.com>
2018-02-13 02:46:02 +0800

    Sync route table configurations

--- vpc/main.tf
+++ vpc/main.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "main" {
   cidr_block = "${var.vpc_cidr_block}"
 
   tags {
-    Name = "main"
+    Name = "${var.role}"
   }
 }
 
@@ -10,6 +10,29 @@ resource "aws_internet_gateway" "main" {
   vpc_id = "${aws_vpc.main.id}"
 
   tags {
-    Name = "main"
+    Name = "${var.role}"
   }
+}
+
+resource "aws_route_table" "main" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.main.id}"
+  }
+
+  tags {
+    Name = "${var.role}"
+  }
+}
+
+resource "aws_route" "main" {
+  route_table_id         = "${aws_route_table.main.id}"
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_main_route_table_association" "main" {
+  vpc_id         = "${aws_vpc.main.id}"
+  route_table_id = "${aws_route_table.main.id}"
 }
