62b871b11924588ebe0e49331e7fa38802cb666f
Robbie Marcelo <rbmrclo@hotmail.com>
2018-02-21 02:43:57 +0800

    Associate public subnets to main route table for explicitness

--- vpc/main.tf
+++ vpc/main.tf
@@ -141,6 +141,21 @@ resource "aws_subnet" "private_c" {
   }
 }
 
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = "${aws_subnet.public_a.id}"
+  route_table_id = "${aws_route_table.main.id}"
+}
+
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = "${aws_subnet.public_b.id}"
+  route_table_id = "${aws_route_table.main.id}"
+}
+
+resource "aws_route_table_association" "public_c" {
+  subnet_id      = "${aws_subnet.public_c.id}"
+  route_table_id = "${aws_route_table.main.id}"
+}
+
 resource "aws_route_table_association" "private_a" {
   subnet_id      = "${aws_subnet.private_a.id}"
   route_table_id = "${aws_route_table.private.id}"
@@ -155,3 +170,4 @@ resource "aws_route_table_association" "private_c" {
   subnet_id      = "${aws_subnet.private_c.id}"
   route_table_id = "${aws_route_table.private.id}"
 }
+
