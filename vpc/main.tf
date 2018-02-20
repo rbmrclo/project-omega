a7ad329875d0d810bd2c232e190edd20e493b386
Robbie Marcelo <rbmrclo@hotmail.com>
2018-02-21 02:40:54 +0800

    Associate private subnets to private route table

--- vpc/main.tf
+++ vpc/main.tf
@@ -140,3 +140,18 @@ resource "aws_subnet" "private_c" {
     Name = "${var.private_subnet_tags["c"]}"
   }
 }
+
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = "${aws_subnet.private_a.id}"
+  route_table_id = "${aws_route_table.private.id}"
+}
+
+resource "aws_route_table_association" "private_b" {
+  subnet_id      = "${aws_subnet.private_b.id}"
+  route_table_id = "${aws_route_table.private.id}"
+}
+
+resource "aws_route_table_association" "private_c" {
+  subnet_id      = "${aws_subnet.private_c.id}"
+  route_table_id = "${aws_route_table.private.id}"
+}
