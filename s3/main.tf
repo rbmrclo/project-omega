a99190b25aaa89b0ce9fa51f3de6da4a4af5df6d
Robbie Marcelo <rbmrclo@hotmail.com>
2018-03-01 02:30:21 +0800

    Setup versioning bucket

--- s3/main.tf
+++ s3/main.tf
@@ -3,6 +3,34 @@ resource "aws_s3_bucket" "public_website_log" {
   acl    = "log-delivery-write"
 }
 
+resource "aws_s3_bucket" "public_website_versioning" {
+  bucket = "${var.domain}-versions"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    prefix  = "config/"
+    enabled = true
+
+    noncurrent_version_transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+
+    noncurrent_version_transition {
+      days          = 60
+      storage_class = "GLACIER"
+    }
+
+    noncurrent_version_expiration {
+      days = 90
+    }
+  }
+}
+
 resource "aws_s3_bucket" "public_website" {
   bucket = "${var.domain}"
   acl    = "private"
@@ -16,6 +44,8 @@ resource "aws_s3_bucket" "public_website" {
     target_prefix = "log/"
   }
 }
+
+
 
 resource "aws_s3_bucket_policy" "public_website" {
   bucket = "${aws_s3_bucket.public_website.id}"
