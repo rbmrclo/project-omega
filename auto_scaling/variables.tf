a2a6ba462acf1b1f30bffef250051906604c28e1
Robbie Marcelo <rbmrclo@hotmail.com>
2018-03-07 02:13:54 +0800

    Setup autoscaling vars

---
+++ auto_scaling/variables.tf
@@ -0,0 +1,18 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "amis" {
+  type = "map"
+
+  default = {
+    us-east-1 = "ami-97785bed" # Amazon Linux
+  }
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "security_group_ids" { type = "list" }
+variable "key_name" {}
